ccd70f6a37ea44a001835ff00eb3938ce1ae9a9d
Kongphai Wutthichaiya <31787868+Kong-TH@users.noreply.github.com>
2025-11-20 11:29:14 +0700

    initial commit

---
+++ Private/SingletonCanDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace KongZEE.Sinagleton.Private
+{
+    public class SingletonCanDestroy<T> : SingletonDontDestroy<T> where T : SingletonCanDestroy<T>
+    {
+        public override void OnAwake()
+        {
+            
+        }
+    }
+}
---
+++ Private/SingletonDontDestroy.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+namespace KongZEE.Sinagleton.Private
+{
+    public class SingletonDontDestroy<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        protected static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        CreateInstance();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public static void CreateInstance()
+        {
+            GameObject obj = new GameObject();
+            obj.name = typeof(T).Name;
+            _instance = obj.AddComponent<T>();
+        }
+        
+        public void Awake()
+        {
+            OnAwake();
+        }
+
+        public virtual void OnAwake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+            }
+            else
+            {
+                if(_instance != this as T)
+                    Destroy(gameObject);
+            }
+            
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
